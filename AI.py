4696af7e2dcde91ed463e2644bd378d015467059
Ahmadreza <46781518+Ahmadrezadl@users.noreply.github.com>
2021-03-31 18:05:03 +0430

    add vision update

--- AI.py
+++ AI.py
@@ -25,10 +25,6 @@ class AI:
     """
 
     def turn(self) -> (str, int, int):
-        print(self.game.mapWidth)
-        print(self.game.mapHeight)
-        print(self.game.ant.currentX)
-        print(self.game.ant.currentY)
         # Fill these fields to return
         message: str = None
         message_value: int = 0
@@ -42,49 +38,51 @@ class AI:
         self.turn_number = self.turn_number + 1
 
         if not self.vision:
+            print("Creating vision")
             for i in range(self.game.mapHeight):
                 new_line = []
                 for j in range(self.game.mapWidth):
-                    if base_x == j and base_y == i:
-                        new_line.append((BASE, self.turn_number))
-                    else:
-                        cell = self.game.ant.visibleMap.cells[i][j]
-                        if not cell:
-                            new_line.append((UNKNOWN, self.turn_number))
-                        elif cell.type == CellType.WALL.value:
-                            new_line.append((WALL, self.turn_number))
-                        elif cell.type == CellType.BASE.value:
-                            new_line.append((ENEMY_BASE, self.turn_number))
-                        elif cell.resource_type != 0:
-                            if cell.resource_type == ResourceType.BREAD.value:
-                                new_line.append((BREAD, self.turn_number))
-                            else:
-                                new_line.append((GRASS, self.turn_number))
-                        elif not cell.ants:
-                            new_line.append((EMPTY, self.turn_number))
-                        else:
-                            maximum = TEAM_KARGAR
-                            for a in cell.ants:
-                                if a.antType == AntType.KARGAR.value and a.antTeam == AntTeam.ALLIED.value:
-                                    this = TEAM_KARGAR
-                                elif a.antType == AntType.SARBAAZ.value and a.antTeam == AntTeam.ALLIED.value:
-                                    this = TEAM_SARBAZ
-                                elif a.antType == AntType.KARGAR.value and a.antTeam == AntTeam.ENEMY.value:
-                                    this = ENEMY_KARGAR
-                                else:
-                                    this = ENEMY_SARBAZ
-
-                                if this > maximum:
-                                    maximum = this
-
-                            new_line.append((maximum, self.turn_number))
-
+                    new_line.append((UNKNOWN, -1))
                 self.vision.append(new_line)
+
+        for i in range(self.game.mapHeight):
+            for j in range(self.game.mapWidth):
+                cell = self.game.ant.getMapCell(j, i)
+                if not cell:
+                    continue
+                elif cell.type == CellType.WALL.value:
+                    self.vision[i][j] = (WALL, self.turn_number)
+                elif cell.type == CellType.BASE.value:
+                    self.vision[i][j] = (ENEMY_BASE, self.turn_number)
+                elif cell.resource_type is not None:
+                    if cell.resource_type == ResourceType.BREAD.value:
+                        self.vision[i][j] = (BREAD, self.turn_number)
+                    else:
+                        self.vision[i][j] = (GRASS, self.turn_number)
+                elif not cell.ants:
+                    self.vision[i][j] = (EMPTY, self.turn_number)
+                else:
+                    maximum = TEAM_KARGAR
+                    for a in cell.ants:
+                        if a.antType == AntType.KARGAR.value and a.antTeam == AntTeam.ALLIED.value:
+                            this = TEAM_KARGAR
+                        elif a.antType == AntType.SARBAAZ.value and a.antTeam == AntTeam.ALLIED.value:
+                            this = TEAM_SARBAZ
+                        elif a.antType == AntType.KARGAR.value and a.antTeam == AntTeam.ENEMY.value:
+                            this = ENEMY_KARGAR
+                        else:
+                            this = ENEMY_SARBAZ
+                        if this > maximum:
+                            maximum = this
+
+                    self.vision[i][j] = (maximum, self.turn_number)
+
 
         if ant.antType == AntType.SARBAAZ.value:
             direction = random.randint(0, 4)
         elif ant.antType == AntType.KARGAR.value:
             pass
 
-        print(self.vision)
+        for i in self.vision:
+            print(i)
         return message, message_value, direction
